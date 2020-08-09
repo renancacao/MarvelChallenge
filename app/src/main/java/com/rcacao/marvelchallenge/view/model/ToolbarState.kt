58af951138f96edc068a562f81740c84e119218c
renan <rcacao10@gmail.com>
2020-08-09 12:36:04 -0300

    remove estado de ui da toolbar nao utilizado

--- app/src/main/java/com/rcacao/marvelchallenge/view/model/ToolbarState.kt
+++ app/src/main/java/com/rcacao/marvelchallenge/view/model/ToolbarState.kt
@@ -1,6 +1,5 @@
 package com.rcacao.marvelchallenge.view.model
 
 sealed class ToolbarState {
-    object DefaultToolbar : ToolbarState()
     data class DetailsToolbar(val name: String, val isFavorite: Boolean) : ToolbarState()
 }
