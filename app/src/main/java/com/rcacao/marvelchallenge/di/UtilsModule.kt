bebd20ca4288de88c801045a7fd630b23fea2455
renan <rcacao10@gmail.com>
2020-08-09 19:55:03 -0300

    extrai interface para ConnectionHelper

--- app/src/main/java/com/rcacao/marvelchallenge/di/UtilsModule.kt
+++ app/src/main/java/com/rcacao/marvelchallenge/di/UtilsModule.kt
@@ -2,6 +2,8 @@ package com.rcacao.marvelchallenge.di
 
 import com.rcacao.marvelchallenge.data.ApiHelper
 import com.rcacao.marvelchallenge.data.ApiHelperImpl
+import com.rcacao.marvelchallenge.utils.ConnectionHelper
+import com.rcacao.marvelchallenge.utils.ConnectionHelperImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -12,4 +14,7 @@ import dagger.hilt.android.components.ApplicationComponent
 abstract class UtilsModule {
     @Binds
     abstract fun bindApiHelper(apiHelperImpl: ApiHelperImpl): ApiHelper
+
+    @Binds
+    abstract fun bindConnectionHelper(connectionHelperImpl: ConnectionHelperImpl): ConnectionHelper
 }
--- app/src/main/java/com/rcacao/marvelchallenge/utils/ConnectionHelper.kt
+++ app/src/main/java/com/rcacao/marvelchallenge/utils/ConnectionHelper.kt
@@ -1,48 +1,7 @@
 package com.rcacao.marvelchallenge.utils
 
-import android.content.Context
-import android.net.ConnectivityManager
-import android.net.ConnectivityManager.NetworkCallback
-import android.net.Network
-import android.net.NetworkInfo
-import android.os.Build
-import dagger.hilt.android.qualifiers.ApplicationContext
-import javax.inject.Inject
-import javax.inject.Singleton
-
-@Singleton
-class ConnectionHelper @Inject constructor(@ApplicationContext private val context: Context) {
-
-    private var isNetworkConnected = false
-
-    fun isConnected(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            isNetworkConnected
-        } else {
-            val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-            activeNetwork?.isConnectedOrConnecting == true
-        }
-    }
-
-    fun registerNetworkCallback() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            try {
-                val connectivityManager =
-                    context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-                connectivityManager.registerDefaultNetworkCallback(object : NetworkCallback() {
-                    override fun onAvailable(network: Network) {
-                        isNetworkConnected = true
-                    }
-
-                    override fun onLost(network: Network) {
-                        isNetworkConnected = false
-                    }
-                })
-                isNetworkConnected = false
-            } catch (e: Exception) {
-                isNetworkConnected = false
-            }
-        }
-    }
+interface ConnectionHelper {
+    var isNetworkConnected: Boolean
+    fun isConnected(): Boolean
+    fun registerNetworkCallback()
 }
